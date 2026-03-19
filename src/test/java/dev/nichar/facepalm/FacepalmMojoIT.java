ddfe60fd7a029296a35baf3ca0b7c4eedd98c077
Nikolas Charalambidis <nikolas.charalambidis@gmail.com>
2026-03-19 23:48:00 +0100

    IT tests

--- src/test/java/dev/nichar/facepalm/FacepalmMojoIT.java
+++ src/test/java/dev/nichar/facepalm/FacepalmMojoIT.java
@@ -5,6 +5,7 @@ import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
 import com.soebes.itf.jupiter.extension.MavenGoal;
 import com.soebes.itf.jupiter.extension.MavenJupiterExtension;
 import com.soebes.itf.jupiter.extension.MavenTest;
+import com.soebes.itf.jupiter.extension.MavenVerbose;
 import com.soebes.itf.jupiter.maven.MavenExecutionResult;
 
 
@@ -23,16 +24,21 @@ class FacepalmMojoIT {
      */
     @MavenTest
     @MavenGoal("verify")
+    @MavenVerbose
     void clean_project(final MavenExecutionResult result) {
         // Verifies the build finished without errors and the scanner log matches expected success.
-        assertThat(result)
-            .isSuccessful()
-            .out().info().contains("SCAN SUCCESS");
+        assertThat(result).isSuccessful();
 
-        // Confirms the HTML report was actually generated in the target directory of the test project.
-        assertThat(result.getMavenProjectResult().getTargetProjectDirectory()
-            .resolve("target/facepalm-report.html"))
-            .exists();
+        assertThat(result).out().info()
+            .contains("Discovered 5 files...")
+            .contains("Starting scan of 4 files...")
+            .contains("SCAN RESULT : SUCCESS")
+            .contains("No secrets or sensitive patterns detected. Your secrets are safe.")
+            .contains("BUILD SUCCESS");
+
+        final var targetDirectory = result.getMavenProjectResult().getTargetProjectDirectory();
+        assertThat(targetDirectory.resolve("target/facepalm-report.html")).exists();
+        assertThat(targetDirectory.resolve("target/facepalm-report.sarif")).exists();
     }
 
     /**
@@ -40,12 +46,28 @@ class FacepalmMojoIT {
      *
      * @param result Maven execution result
      */
-    //@MavenTest
+    @MavenTest
+    @MavenGoal("verify")
+    @MavenVerbose
     void dirty_project(final MavenExecutionResult result) {
-        // Build should succeed if failOnError is false, but log finding details.
-        assertThat(result)
-            .isSuccessful()
-            .out().info().contains("SCAN FAILURE").contains("AWS Access Key");
+        // Verifies the build finished without errors and the scanner log matches expected success.
+        assertThat(result).isSuccessful();
+
+        assertThat(result).out().warn()
+            .contains("[Generic Password Assignment] Score: 69.8 (R:85/C:60) - \uD83D\uDFE1");
+        assertThat(result).out().info()
+            .contains("  Location: /Users/nikolas/Projects/Personal/facepalm-maven-plugin/target/maven-it/dev/nichar/facepalm/FacepalmMojoIT/dirty_project/project/src/main/resources/application.properties:2");
+
+        assertThat(result).out().info()
+            .contains("Discovered 5 files...")
+            .contains("Starting scan of 4 files...")
+            .contains("SCAN RESULT : WARNINGS")
+            .contains("Warnings detected. Review recommended.")
+            .contains("BUILD SUCCESS");
+
+        final var targetDirectory = result.getMavenProjectResult().getTargetProjectDirectory();
+        assertThat(targetDirectory.resolve("target/facepalm-report.html")).exists();
+        assertThat(targetDirectory.resolve("target/facepalm-report.sarif")).exists();
     }
 
     /**
@@ -53,9 +75,27 @@ class FacepalmMojoIT {
      *
      * @param result Maven execution result
      */
-    //@MavenTest
-    void dirty_project_fail_on_error(final MavenExecutionResult result) {
-        // Assert that the Maven process exited with a non-zero status code.
+    @MavenTest
+    @MavenGoal("verify")
+    @MavenVerbose
+    void dirty_project_fail_on_warnings(final MavenExecutionResult result) {
+        // Verifies the build finished with errors and the scanner log matches expected success.
         assertThat(result).isFailure();
+
+        assertThat(result).out().warn()
+            .contains("[Generic Password Assignment] Score: 69.8 (R:85/C:60) - \uD83D\uDFE1");
+        assertThat(result).out().info()
+            .contains("  Location: /Users/nikolas/Projects/Personal/facepalm-maven-plugin/target/maven-it/dev/nichar/facepalm/FacepalmMojoIT/dirty_project_fail_on_warnings/project/src/main/resources/application.properties:2");
+
+        assertThat(result).out().info()
+            .contains("Discovered 5 files...")
+            .contains("Starting scan of 4 files...")
+            .contains("SCAN RESULT : WARNINGS")
+            .contains("Warnings detected. Review recommended.")
+            .contains("BUILD FAILURE");
+
+        final var targetDirectory = result.getMavenProjectResult().getTargetProjectDirectory();
+        assertThat(targetDirectory.resolve("target/facepalm-report.html")).exists();
+        assertThat(targetDirectory.resolve("target/facepalm-report.sarif")).exists();
     }
 }
